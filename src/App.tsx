219934fcd7d97b709155511f48e0e20222a3fa43
DavidKhachikyan <davidxachikyan28@gmail.com>
2024-08-03 17:29:26 +0400

    Fix redux state saving

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "antd";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import Todo from "./pages/Todo/Todo";
 import TrashCan from "./pages/TrashCan/TrashCan";
 import HeaderApp from "./components/Header/Header";
+import store, { persistor } from "./store/store";
 
 const { Content } = Layout;
 
 const App: React.FC = () => {
   return (
-    <Layout className="min-h-screen">
-      <HeaderApp />
-      <Content className="p-5">
-        <Routes>
-          <Route path="/" element={<Todo />} />
-          <Route path="/trash" element={<TrashCan />} />
-        </Routes>
-      </Content>
-    </Layout>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Layout className="min-h-screen">
+          <HeaderApp />
+          <Content className="p-5">
+            <Routes>
+              <Route path="/" element={<Todo />} />
+              <Route path="/trash" element={<TrashCan />} />
+            </Routes>
+          </Content>
+        </Layout>
+      </PersistGate>
+    </Provider>
   );
 };
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Default localStorage
 import todoReducer from '../features/todos/todoSlice';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, todoReducer);
 
 const store = configureStore({
     reducer: {
-        todos: todoReducer,
+        todos: persistedReducer,
     },
 });
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 
 export default store;
-
-
-
